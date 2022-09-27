83659bfa279780b072c4a04af241ca276e91af5c
celadia <celadia@21-school.ru>
2022-09-27 17:11:17 +0300

    finish module 07

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,6 +1,102 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+#include <ostream>
+#include <iostream>
 
+template <typename T>
+class Array
+{
+public:
+	Array();
+	Array(unsigned int n);
+	Array(Array const &toCopy);
+
+	~Array();
+
+	Array &operator = (Array const &toCopy);
+	T &operator [] (const int &i) const;
+
+	int size() const;
+
+	class IndexOutOfBounds : public std::exception
+	{
+	public:
+		const char* what() const throw();
+	};
+
+private:
+	unsigned int _size;
+	T *_arr;
+};
+
+template <typename T>
+Array<T>::Array()
+{
+	this->_size = 0;
+	this->_arr = new T[0];
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+	this->_size = n;
+	this->_arr = new T[this->_size];
+}
+
+template <typename T>
+Array<T>::Array(Array const &toCopy)
+{
+	this->_size = toCopy._size;
+	this->_arr = new T[this->_size];
+
+	for (unsigned int i = 0; i < this->_size; i++)
+		this->_arr[i] = toCopy._arr[i];
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] this->_arr;
+}
+
+template <typename T>
+Array<T> &Array <T>::operator = (const Array &toCopy)
+{
+	delete [] this->_arr;
+
+	if (toCopy.size() != 0)
+	{
+		this->_size = toCopy.size();
+		this->_arr = new T[this->_size];
+
+		for (unsigned int i = 0; i < this->_size; i++)
+			this->_arr[i] = toCopy[i];
+	}
+
+	return *this;
+}
+
+template <typename T>
+T &Array<T>::operator [] (const int &i) const
+{
+	if (i < 0 || i >= static_cast<int>(this->_size))
+		throw IndexOutOfBounds();
+
+	return this->_arr[i];
+}
+
+template <typename T>
+int Array<T>::size() const
+{
+	return this->_size;
+}
+
+template <typename T>
+const char *Array<T>::IndexOutOfBounds::what() const throw()
+{
+	return "Index out of bounds";
+}
 
 #endif //ARRAY_HPP
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,47 +1,77 @@
-#include <iostream>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
-{	Array<int> numbers(MAX_VAL);	int* mirror = new int[MAX_VAL];	srand(time(NULL));	for (int i = 0; i < MAX_VAL; i++)
+class TestClass
+{
+public:
+	TestClass() : _number(42) {}
+
+	int getNumber() const
 	{
-		const int value = rand();
-		numbers[i] = value;
-		mirror[i] = value;
+		return this->_number;
 	}
-	//SCOPE
-	{
-		Array<int> tmp = numbers;
-		Array<int> test(tmp);
-	}
-	for (int i = 0; i < MAX_VAL; i++)
-	{
-		if (mirror[i] != numbers[i])
-		{
-			std::cerr << "didn't save the same value!!" << std::endl;
-			return 1;
-		}
-	}
+
+private:
+	int _number;
+};
+
+int main()
+{
+	Array<TestClass>	arrayClass(5);
+	Array<int>			arrayInt(3);
+	Array<std::string>	arrayString(10);
+	Array<int>			empty;
+
+	std::cout << " -- Testing operator [] --" << std::endl;
+	arrayInt[2] = 2022;
+	arrayString[9] = "Testing some string";
+	std::cout << arrayInt[2] << std::endl;
+	std::cout << arrayString[9] << std::endl;
+	std::cout << arrayClass[4].getNumber() << std::endl;
+	std::cout << "-------------------" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << " -- Testing array size --" << std::endl;
+	std::cout << empty.size() << std::endl;
+	std::cout << arrayInt.size() << std::endl;
+	std::cout << arrayString.size() << std::endl;
+	std::cout << arrayClass.size() << std::endl;
+	std::cout << "-------------------" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << " -- Testing operator = --" << std::endl;
+	std::cout << "Empty size before: " << empty.size() << std::endl;
+	std::cout << "Assigning arrayInt to empty" << std::endl;
+	empty = arrayInt;
+	std::cout << "Empty size after: " << empty.size() << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Values of empty[2] and arrayInt[2]:" << std::endl;
+	std::cout << empty[2] << " and " << arrayInt[2] << std::endl;
+	std::cout << "Changing value of empty[2]" << std::endl;
+	empty[2] = 999;
+	std::cout << "Values of empty[2] and arrayInt[2]:" << std::endl;
+	std::cout << empty[2] << " and " << arrayInt[2] << std::endl;
+	std::cout << "-------------------" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << " -- Testing exceptions --" << std::endl;
 	try
 	{
-		numbers[-2] = 0;
+		std::cout << arrayInt[10] << "This should not be printed ;)" << std::endl;
 	}
-	catch(const std::exception& e)
+	catch(std::exception &e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cout << "Exception caught: " << e.what() << std::endl;
 	}
+
 	try
 	{
-		numbers[MAX_VAL] = 0;
+		std::cout << arrayClass[-1].getNumber() << "This should not be printed ;)" << std::endl;
 	}
-	catch(const std::exception& e)
+	catch(std::exception &e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cout << "Exception caught: " << e.what() << std::endl;
 	}
-	for (int i = 0; i < MAX_VAL; i++)
-	{
-		numbers[i] = rand();
-	}
-	delete [] mirror;//
-	return 0;
+
+	return (0);
 }
